7495b09380a5493ba522c9d254089ee0cc4f9507
troublee <canon2705@gmail.com>
2020-08-29 21:41:07 +0300

    1 file deleted

--- src/notes.js
+++
@@ -1,8 +0,0 @@
-const notesList = [
-  { text: 'Real-Time', pinned: false, tags: ['11', '22'] },
-  { text: 'Audience', pinned: false, tags: [] },
-  { text: 'Conversions', pinned: false, tags: [] },
-  { text: 'Real-Time', pinned: false, tags: [] }
-]
-
-export default notesList
